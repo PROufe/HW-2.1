41894312d7add063cb9c3f15f1ddb68ee5e0d438
Yuri <smart.money96@gmail.com>
2020-08-23 17:41:31 +0500

    Initial Commit

---
+++ HW 2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2.1
+//
+//  Created by Яна Варфоломеева on 23.08.2020.
+//  Copyright © 2020 Grigorev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
